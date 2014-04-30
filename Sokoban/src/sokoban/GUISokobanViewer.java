40d7addb718b9aabffbf9b336ad7ed5ba0271d14
Lohana Lema Moreta. <lohana.8@gmail.com>
2014-05-01 00:44:58 +0200

    Skeleton for Sokoban Project

---
+++ Sokoban/src/sokoban/GUISokobanViewer.java
@@ -0,0 +1,34 @@
+package sokoban;
+
+public class GUISokobanViewer extends SokobanViewer{
+
+	public GUISokobanViewer(Board board) {
+		super(board);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void drawPlayer(IPosition position) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drawEmptyField(IPosition position, boolean isFinalState) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drawBox(IPosition position, boolean isFinalState) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drawWall(IPosition position) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
